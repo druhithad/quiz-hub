e357e1ecec78b1ef0add85e905d2e78ccd523cf4
druhithad <druhithad@gmail.com>
2026-04-18 11:49:08 +0530

    Added Firebase services, routes, and app setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Home from "../pages/Home";
+import QuizPage from "../pages/QuizPage";
+import Result from "../pages/Result";
+import Leaderboard from "../pages/Leaderboard";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
